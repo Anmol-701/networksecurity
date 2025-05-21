0210bce81b96dc4e8ecd791afbf26cac09833222
Anmol-701 <anmolrana909@gmail.com>
2025-05-21 09:27:43 +0530

    data validation completed

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,6 +1,6 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-uri = "mongodb+srv://anmolpisoft:01eB8ETheoMtg7oO@cluster0.cv5du.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://anmolpisoft:5dxKC1RN7Ua6I0ym@cluster0.cv5du.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
